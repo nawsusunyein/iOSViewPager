e6c12d7c5cca65c9bba818118aaebd7e42fc65c9
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-12-30 15:37:09 +0630

    put 3 tabs with few text

--- iOSViewPager/iOSViewPager/ViewController.m
+++ iOSViewPager/iOSViewPager/ViewController.m
@@ -19,8 +19,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.dataSource = self;
-    self.webUrlLinks = @[@"https://www.google.com",@"https://www.facebook.com",@"https://www.yahoo.com",@"https://www.google.com",@"https://www.facebook.com",@"https://www.yahoo.com",@"https://www.google.com",@"https://www.facebook.com"];
-    NSArray *data = @[@"News",@"キャンペーン",@"閲覧履歴",@"ランキング",@"新着",@"TSC",@"White",@"UL"];
+    self.webUrlLinks = @[@"https://www.google.com",@"https://www.facebook.com",@"https://www.gmail.com"];
+    NSArray *data = @[@"News",@"キャンペーン",@"閲覧履歴"];
     [self setTabData:data];
 }
 
