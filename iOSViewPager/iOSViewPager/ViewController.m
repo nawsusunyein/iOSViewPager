0ada3820031628297a7fb6fd50b459e9348ad1f8
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-12-30 15:09:39 +0630

    remove unnecessary code

--- iOSViewPager/iOSViewPager/ViewController.m
+++ iOSViewPager/iOSViewPager/ViewController.m
@@ -19,13 +19,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.dataSource = self;
-    NSString *campaign = @"キャンペーン";
-    NSLog(@"campaign count %lu",(unsigned long)campaign.length);
-    
     self.webUrlLinks = @[@"https://www.google.com",@"https://www.facebook.com",@"https://www.yahoo.com",@"https://www.google.com",@"https://www.facebook.com",@"https://www.yahoo.com",@"https://www.google.com",@"https://www.facebook.com"];
     NSArray *data = @[@"News",@"キャンペーン",@"閲覧履歴",@"ランキング",@"新着",@"TSC",@"White",@"UL"];
     [self setTabData:data];
-    // Do any additional setup after loading the view.
 }
 
 - (UIViewController *)viewPager:(UIViewController *)viewPager contentViewControllerForTabAtIndex:(NSUInteger)index {
