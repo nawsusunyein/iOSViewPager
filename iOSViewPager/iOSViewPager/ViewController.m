668ab39a28b8f041b84cdfbbad2b69a1f05b2e5b
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-12-30 16:31:18 +0630

    put 40 words for eight tabs

--- iOSViewPager/iOSViewPager/ViewController.m
+++ iOSViewPager/iOSViewPager/ViewController.m
@@ -20,7 +20,7 @@
     [super viewDidLoad];
     self.dataSource = self;
     self.webUrlLinks = @[@"https://www.google.com",@"https://www.facebook.com",@"https://www.yahoo.com",@"https://www.google.com",@"https://www.facebook.com",@"https://www.yahoo.com",@"https://www.google.com",@"https://www.facebook.com"];
-    NSArray *data = @[@"News",@"キャンペーン",@"閲覧履歴",@"ランキング",@"新着",@"TSC",@"White",@"UL"];
+    NSArray *data = @[@"News News News News News News News New40",@"キャンペーン キャンペーン キャンペーン キャンペーン キャンペーン キャン40",@"閲覧履歴 閲覧履歴 閲覧履歴 閲覧履歴 閲覧履歴 閲覧履歴 閲覧履歴 閲覧履40",@"ランキング ランキング ランキング ランキング ランキング ランキング ラン40",@"新着 新着 新着 新着新着 新着 新着 新着 新着 新着 新着 新着 新着 40",@"TSC TSC TSC TSC TSC TSC TSC TSC TSC TS40",@"White White White White White White Wh40",@"UL UL UL UL UL UL UL UL UL UL UL UL UL40"];
     [self setTabData:data];
 }
 
